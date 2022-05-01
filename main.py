d1a808b1697d441e551714e094b0422e31c75a0a
JMurphy7 <46505040+JMurphy7@users.noreply.github.com>
2022-05-01 16:09:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from operator import truediv
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
@@ -14,8 +15,10 @@ from cv_bridge import CvBridge
 from cv2 import COLOR_BGR2GRAY, waitKey
 import numpy 
 import cv2
+import random
 class robot:
     priority = False
+    goal_in_sight = False
     def __init__(self):
         # https://github.com/LCAS/teaching/blob/lcas_melodic/cmp3103m-code-fragments/scripts/opencv_bridge.py
         self.bridge = CvBridge()
@@ -30,118 +33,146 @@ class robot:
         
         # 5 hz
         r = rospy.Rate(5)
-        
-        # Move command is a twist command, giving us control of direction
-        #move_cmd = Twist()
-        #move_cmd.linear.x = 0.1
-        # While rospy is running, keep moving
-        #while not rospy.is_shutdown():
-        #    self.pub.publish(move_cmd)
+
+      
+    def go_for_the_goal(self,F,mask3,w):
+        # The final dash that tunnel visions on the goal, dashing straight for it using modified 'line follower' code.
+        if F['m00'] == 0:
+            print("Mission complete.")
             
+        F = cv2.moments(mask3)
+        gx = int(F['m10']/F['m00'])
+        gy = int(F['m01']/F['m00'])
+            
+        err = gx - w/2
+        self.twist.linear.x = 0.3
+        self.twist.angular.z = -float(err) / 100
+        self.pub.publish(self.twist)
+        self.priority = True
+          
     def image_callback(self, data):
         #namedWindow("Image window")
         
         cv2.namedWindow("Image window", 1)
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        #gray_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image3 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         #hsv = cv_image
         lower_yellow = numpy.array([0, 100, 100])
         upper_yellow = numpy.array([0, 255, 255])
         
-        #lower_red = numpy.array([0, 170, 0])
-        #upper_red = numpy.array([0, 255, 0])
         lower_red = numpy.array([0, 0, 0])
         upper_red = numpy.array([10, 50, 255])
+
+        lower_green = numpy.array([0, 100, 0])
+        upper_green = numpy.array([0, 255, 0])
        
+        lower_blue = numpy.array([100, 0, 0])
+        upper_blue = numpy.array([255, 0, 0])
         mask = cv2.inRange(cv_image, lower_yellow, upper_yellow)
-        mask2 = cv2.inRange(cv_image2,lower_red,upper_red)
-                
-        #cv2.bitwise_and(cv_image, cv_image, mask=mask)
-        #cv2.bitwise_and(cv_image2,cv_image2,mask=mask2)
+        mask2 = cv2.inRange(cv_image2,lower_red, upper_red)
+        mask3 = cv2.inRange(cv_image3,lower_green, upper_green)
+        mask4 = cv2.inRange(cv_image,lower_blue, upper_blue)
+
         h, w, d = cv_image.shape
         search_top = h/15
         search_bot = search_top + 20
+        #For testing masks
+        cv_image2[h/4*3:,0:-1] = 0 
         
-        #mask[0:search_top, 0:w] = 0
         mask[0:h, 0:w/5] = 0
         mask[0:h, (w/5)*4:-1] = 0
-        mask2[-1:(h/5)*4, 0:w] = 0
-        #mask[0:h, 0+(w/20):w/5] = 0
-        #mask[0:h, (w/5)*4:w-(w/20)] = 0
-        #mask[0:h, 0:w/5] = 0
-        #mask[0:h, (w/5)*4:w] = 0
         
-        #mask[search_bot:h, 0:w] = 0
-        
-        # Taken from https://github.com/LCAS/teaching/blob/lcas_melodic/ros_book_line_follower/src/follower_p.py
+        mask2[h/4*3:, 0:-1] = 0
 
-        M = cv2.moments(mask)
-        # Bascially the same as the yellow line, but reverse engineered to run AWAY from red squares
-        F = cv2.moments(mask2)
-        # Focus on homing on in the yellow line
-        if F['m00'] > 0:
-            gx = int(F['m10']/F['m00'])
-            gy = int(F['m01']/F['m00'])
-            
-            err = gx - w/2
-            self.twist.linear.x = 0.1
-            self.twist.angular.z = float(err) / 100
-            self.pub.publish(self.twist)
-            self.priority = True
+
+        if(self.goal_in_sight == True):
+            try:
+                F = cv2.moments(mask3)
+                self.go_for_the_goal(F,mask3,w)
+            except:
+                print("whoops")
         else:
-            self.priority = False
-        if M['m00'] > 0:
-            #count = 0
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            
-            cv2.circle(cv_image, (cx, cy), 20, (0, 0, 255), -1)
-            if self.priority == False:
-                err = cx - w/2
+            # Taken from https://github.com/LCAS/teaching/blob/lcas_melodic/ros_book_line_follower/src/follower_p.py
+
+            M = cv2.moments(mask)
+            M2 = cv2.moments(mask4)
+            # Bascially the same as the yellow line, but reverse engineered to run AWAY from red squares
+            F = cv2.moments(mask2)
+            if F['m00'] > 0:
+                gx = int(F['m10']/F['m00'])
+                gy = int(F['m01']/F['m00'])
+                
+                err = gx - w/2
                 self.twist.linear.x = 0.1
-                self.twist.angular.z = -float(err) / 100
+                self.twist.angular.z = float(err) / 80
                 self.pub.publish(self.twist)
-        else: # If a yellow line cannot be found, rotate on the spot until one is found, favouring a left turn 
-            if self.priority == False:
-                self.twist.linear.x = -0.1
-                self.pub.publish(self.twist)
-            #count = count + 0.1
-            #if(count > 90):
-            #self.twist.linear.x = 0
-            #self.twist.angular.z= -0.1
-            #else:
-            #    self.twist.linear.x = 0
-            #    self.twist.angular.z= 0.1
-            #self.twist.linear.x = 0.2 # Make it check left and right?
-            #self.pub.publish(self.twist)
-        imshow("Image window", mask2)
+                self.priority = True
+            else:
+                self.priority = False
+            # And now again but for green. This one, however, will mindlessly dash for the green tile
+            
+            F = cv2.moments(mask3)
+            if F['m00'] > 0:
+                self.goal_in_sight = True
+
+            # Focus on homing on in the yellow line
+            if M['m00'] > 0:
+                #count = 0
+
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                
+                cv2.circle(cv_image, (cx, cy), 20, (0, 0, 255), -1)
+                if self.priority == False:
+                    err = cx - w/2
+                    self.twist.linear.x = 0.1
+                    self.twist.angular.z = -float(err) / 100
+                    if M2['m00'] > 0:
+                        cx = int(M2['m10']/M2['m00'])
+                        cy = int(M2['m01']/M2['m00'])
+                        
+                        cv2.circle(cv_image, (cx, cy), 20, (0, 0, 255), -1)
+                        err = cx - w/2
+                        self.twist.linear.x = 0.1
+                        self.twist.angular.z = -float(err) / 2000
+                    self.pub.publish(self.twist)
+            else: # If a yellow line cannot be found, rotate on the spot until one is found
+                if self.priority == False:
+                    #random.seed(self.twist.linear.x)
+                    a = random.randint(1,10)
+                    if(a < 5):
+                        print("spinning")
+                        self.twist.angular.z = 0.6
+                        self.pub.publish(self.twist)
+                    else:
+                        print("spinning aaaa")
+                        self.twist.angular.z = -0.6
+                        self.pub.publish(self.twist)
+        imshow("Image window", mask)
         waitKey(1)
 
     def scan_and_search(self,laser_msg):
         # Code from https://github.com/LCAS/teaching/blob/13e519fd645970c61e54f5bc3d1ec524bb9cfd9a/cmp3103m-code-fragments/scripts/lecture_02.py
         a = len(laser_msg.ranges)
         # 0:106, 106:212,  212:320
-        if laser_msg.ranges[a/3] < 1.0:
+        if laser_msg.ranges[a/3] < 0.5:
+            print("Oh no! A wall!")
             self.priority = True
             self.twist.linear.x = 0
-            self.twist.angular.z= -0.5
+            self.twist.angular.z= -0.2
             self.pub.publish(self.twist)
-        #elif laser_msg.ranges[a/3:(a/3)*2] < 0.1:
-            #self.priority = True
-            #self.twist.linear.x = -0.3
-            #self.pub.publish(self.twist)
-        elif laser_msg.ranges[(a/3*2):-1] < 1.0:
+        elif laser_msg.ranges[(a/3*2):-1] < 0.5:
+            print("Oh no! A wall!")
             self.priority = True
             self.twist.linear.x = 0
-            self.twist.angular.z= 0.5
+            self.twist.angular.z= 0.2
             self.pub.publish(self.twist)
         else:
             self.priority = False
-        #    t = Twist()
-        #    t.linear.x = 1.0
-        #    self.pub.publish(t)
+
 
     # Step one, find wall, follow wall.
     # Step two, check for blue squares, if a blue square is spotted, override step one and move to the blue sqaure
